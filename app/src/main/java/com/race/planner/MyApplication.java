4cb793c9de07b1f88722d937fe27166d7a54542d
Ryan Page <rpage101@gmail.com>
2017-05-16 11:59:58 -0700

    created new instructions ui

---
+++ app/src/main/java/com/race/planner/MyApplication.java
@@ -0,0 +1,16 @@
+package com.race.planner;
+
+import android.app.Application;
+
+/**
+ * Created by dev on 5/16/2017.
+ */
+
+public class MyApplication extends Application
+{
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+    }
+}
